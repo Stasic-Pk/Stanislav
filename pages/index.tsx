398fe1de3605bda07d7e91ec94a8ebd193ce462a
Стасік Петрмк <stasps210@gmail.com>
2022-12-22 17:04:20 +0200

    create my site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,34 @@
+import Image from "next/image";
 import { GitHubIcon } from "../components/icons/GitHubIcon"
+import { TelegramIcon } from "../components/icons/TelegramIcon"
+import { TwitterIcon } from "../components/icons/TwitterIcon"
+
+import forest from '../public/forest.jpg';
 
 export default function Home() {
+  
   return (
-    <div>
-      <GitHubIcon />
+    <div className="box">
+      <div className="boxChildren">
+        <div className="text">Stasnislav <br/> Petryk</div>
+        <div className="line"></div>
+
+        <div className="icons">
+          <div className="icon"><GitHubIcon /></div>
+          <div className="icon"><TelegramIcon /></div>
+          <div className="icon"><TwitterIcon /></div>
+        </div>
+      </div>
+      <div className="imgWrapper">
+        <Image
+          priority={true}
+          src={forest.src}
+          alt="forest picture"
+          width={3500}
+          height={3500}
+          className="forestImage"
+        />
+      </div>
     </div>
   )
 }
