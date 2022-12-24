da3c6652f8fd6178300c573cdffdb88f3c0593f9
Oleksandr Petryk <sachapos50@gmail.com>
2022-12-24 22:05:11 +0200

    feat: responsive layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,34 +5,35 @@ import { TwitterIcon } from "../components/icons/TwitterIcon"
 
 import forest from '../public/forest.jpg';
 
-export default function Home() {
-  
+export default function Home() { 
   return (
-    <div className="box">
-      <div className="boxChildren">
-        <div>
-          <div className="text">Stasnislav <br/> Petryk</div>
-          <div className="line"></div>
+    <div className='baseWrapper'>
+      <div className="box">
+        <div className="boxChildren">
+          <div>
+            <div className="text">Stasnislav <br/> Petryk</div>
+            <div className="line"></div>
+          </div>
+
+          <div className="icons">
+            <div className="icon"><GitHubIcon /></div>
+            <div className="icon"><TelegramIcon /></div>
+            <div className="icon"><TwitterIcon /></div>
+          </div>
         </div>
 
-        <div className="icons">
-          <div className="icon"><GitHubIcon /></div>
-          <div className="icon"><TelegramIcon /></div>
-          <div className="icon"><TwitterIcon /></div>
+        <div className="imgWrapper">
+          <Image
+            priority={true}
+            src={forest.src}
+            alt="forest picture"
+            width={3500}
+            height={3500}
+            className="forestImage"
+          />
+
+          <div className="imgFilter"></div>
         </div>
-      </div>
-
-      <div className="imgWrapper">
-        <Image
-          priority={true}
-          src={forest.src}
-          alt="forest picture"
-          width={3500}
-          height={3500}
-          className="forestImage"
-        />
-
-        <div className="imgFilter"></div>
       </div>
     </div>
   )
